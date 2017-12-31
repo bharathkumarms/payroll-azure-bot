51f63e07f1ab14356b0e3ef0442bbe5e6f134fa1
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2017-12-31 23:19:31 +0530

    Added post call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Headers, Response, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is done. Second Time!';
+  constructor(private http: Http) { }
+
+  answer() {
+
+    var headers = new Headers();
+    headers.append('Ocp-Apim-Subscription-Key', 'f741f4e6c6254d9dbc80f85d16fd30fb');
+    let urlSearchParams = new URLSearchParams();
+    urlSearchParams.append('question', 'hi');
+    let body = urlSearchParams.toString()
+    
+    this.http.post('https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/d2f5139b-5a23-4049-8893-27fecb715f59/generateAnswer', body, {headers: headers})
+        .map((response: Response) => {
+            // login successful if user.status = success in the response
+            let user = response.json();
+        });
+  }
 }
