bd75065561350a03b2340b635ed6ef699ef93182
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2018-01-01 11:12:22 +0530

    Improvements to bot ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,32 +23,33 @@ export class AppComponent implements AfterViewChecked {
   @ViewChildren('input') vc;
 
   constructor(private http: Http) {
+    this.question = ""
     this.placeholder = true;
     this.errorDisplay = false;
     this.timer = TimerObservable.create(0, 1000);
   }
 
-  ngAfterViewChecked() {    
-    console.log("lllllllllllllllllllll");
-    console.log(this.vc);       
+  ngAfterViewChecked() {   
     this.vc.first.nativeElement.focus();
 }
 
   answer() {
     this.qa.unshift('<div class="user-question"><b>Me:</b> ' + this.question + '</div>');
     this.loading = true
+    var actualQuestion = this.question
+    this.question = ""
     this.placeholder = false;
     var headers = new Headers();
     headers.append('Ocp-Apim-Subscription-Key', 'f741f4e6c6254d9dbc80f85d16fd30fb');
 
 
-    this.http.post("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/d2f5139b-5a23-4049-8893-27fecb715f59/generateAnswer", { 'question': this.question }, { headers: headers })
+    this.http.post("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/d2f5139b-5a23-4049-8893-27fecb715f59/generateAnswer", { 'question': actualQuestion }, { headers: headers })
       .map(response => response.json())
       .subscribe(
       value => (console.log(value),
         this.loading = false,
-        this.qa.unshift('<div class="adp-answer"><b>Bot:</b> ' + value.answers[0].answer + '</div>'),
-        this.question = ""),
+        this.qa.unshift('<div class="adp-answer"><b>Bot:</b> ' + value.answers[0].answer + '</div>')
+        ),
       error => (console.log(error),
         this.qa.unshift('<div class="adp-error"><b>Bot:</b> ' + 'Please try next question in 30-60 sec. Too many request or check your network.' + '</div>'),
         this.errorDisplay = true,
